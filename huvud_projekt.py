00d0e73cffdeaf1497361fb52901e471c02c6f5e
korv9 <anton.ernstson@gmail.com>
2024-10-15 09:59:31 +0200

    andra commit

--- huvud_projekt.py
+++ huvud_projekt.py
@@ -12,7 +12,7 @@ class GUI: #klass för tkinter GUI,
     def __init__(self) -> None:
         self.root=tkinter.Tk()
         self.root.title("weather app")
-        self.root.geometry("1500x1500")
+        self.root.geometry("800x800")
         self.text_label=tkinter.Label(self.root,text="welcome to app")
         self.text_label.pack()
         
@@ -20,14 +20,14 @@ class GUI: #klass för tkinter GUI,
         self.bg_label.place(relwidth=1, relheight=1)
         
         # värden för att centrera labels o buttons
-        window_width = 1500 
+        window_width = 800
         label_width = 300 
         button_width = 160 
         entry_width = 160
         
         self.city_name = tkinter.StringVar() #sparar det man skriver i boxen 
         self.name_entry = tkinter.Entry(self.root,textvariable = self.city_name,  width=20, font=('calibre',10,'normal'))
-        self.name_entry.place(x=(window_width - entry_width) //2 , y=800) #bakar in formeln window width - label_width / 2 för att få det centrerat
+        self.name_entry.place(x=(window_width - entry_width) //2 , y=550) #bakar in formeln window width - label_width / 2 för att få det centrerat
 
         label_width = 300 # värde för label pixel bredd, för att kunna centrera dom
         button_width = 160 # värde för button bredd
@@ -42,10 +42,11 @@ class GUI: #klass för tkinter GUI,
         #self.image_label.pack(pady=10)
         
         self.fetch_button = tkinter.Button(self.root, text="Get Weather",width=20, command=lambda: self.fetch_weather()) 
-        self.fetch_button.place(x=(window_width - button_width)//2 - button_width, y=900)
+        self.fetch_button.place(x=(window_width - button_width)//2 - button_width, y=650)
+       # self.fetch_button.bind("<Enter>", fetch_weather())
 
         self.random_city_button = tkinter.Button(self.root, text="surprise city....",width=20, command=lambda: self.fetch_random_city_weather())
-        self.random_city_button.place(x=(window_width - button_width)//2 + button_width, y=900)
+        self.random_city_button.place(x=(window_width - button_width)//2 + button_width, y=650)
 
 
 
@@ -60,7 +61,7 @@ class GUI: #klass för tkinter GUI,
         city.get_picture()
         
     def fetch_random_city_weather(self): #funktion för att hämta random stad från lista
-        cities_file_path = "stor_projekt\cities_list.txt"
+        cities_file_path = "mitt_projekt\cities_list.txt"
         
         if os.path.exists(cities_file_path): #kollar om filen finns
             
@@ -84,7 +85,7 @@ class City: #klass för att strukturera upp väderdatan stad för stad
         
  
     def save_city(self): #funktion att spara städer för att sen randomizea 
-        cities_file_path = "stor_projekt\cities_list.txt"
+        cities_file_path = "mitt_projekt\cities_list.txt"
           
         if not os.path.exists(cities_file_path): #skapar fil om den inte finns
           open(cities_file_path, 'w').close()
@@ -153,7 +154,7 @@ class City: #klass för att strukturera upp väderdatan stad för stad
                     # tar satellit bilden och öppnar bilden 
                     image = Image.open(BytesIO(response_nasa.content))
                     
-                    image = image.resize((1500, 1500),)# Image.ANTIALIAS)  # minskar storleken på bilden
+                    image = image.resize((800, 800),)# Image.ANTIALIAS)  # minskar storleken på bilden
                     img_tk = ImageTk.PhotoImage(image) # konverterar bilden med photoimage så den kommer fram i tkinter
 
                     #tkinter label för bilden
@@ -179,7 +180,7 @@ class City: #klass för att strukturera upp väderdatan stad för stad
 
 
 
-with open("stor_projekt/api_key.txt","r") as API_file:   #öppnar API_key filen o läser de två nycklarna
+with open("mitt_projekt/api_key.txt","r") as API_file:   #öppnar API_key filen o läser de två nycklarna
              API_keys = API_file.readlines()
              API_keys = [key.strip() for key in API_keys]  # tar bort tomrum 
              API_key_weather = API_keys[0] # läser rad 1 som är API nyckeln till openweater
